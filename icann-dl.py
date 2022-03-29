699b74cb25dd6a0cec52472b9d20256807dc4c40
smutt <smutt@depht.com>
2022-03-29 12:38:21 +0000

    Many changes. Added Board corespondence and GAC

--- icann-dl.py
+++ icann-dl.py
@@ -22,95 +22,157 @@ import stat
 import re
 import requests
 from bs4 import BeautifulSoup
+from urllib3 import util as Util
 
 base_dir = '/home/smutt/www/depht.com/icann/pubs/'
 
+exclude = []
+exclude.append(re.compile('.*/didp-response-process-29oct13-en\.pdf$'))
+exclude.append(re.compile('.*/registrar-billing-faq-21dec21-en\.pdf$'))
+exclude.append(re.compile('.*/gdd-ops-handbook-registry-operators-15aug18-en\.pdf$'))
+exclude.append(re.compile('.*/rsep-process-workflow-14jun19-en\.pdf$'))
+exclude.append(re.compile('.*/mosapi-specification\.pdf$'))
+
 groups = {}
 groups['ssac'] = {}
-groups['ssac']['uri']  = []
-groups['ssac']['uri'].append('https://www.icann.org/groups/ssac/documents')
+groups['ssac']['uri'] = 'https://www.icann.org/groups/ssac/documents'
 groups['ssac']['regex'] = []
 groups['ssac']['regex'].append(re.compile('.*/groups/ssac/documents/sac-.*\.pdf$'))
 groups['ssac']['regex'].append(re.compile('.*/system/files/files/sac-.*\.pdf$'))
 
 groups['ssac_cor'] = {}
-groups['ssac_cor']['uri'] = []
-groups['ssac_cor']['uri'].append('https://www.icann.org/groups/ssac/documents-correspondence')
+groups['ssac_cor']['uri'] = 'https://www.icann.org/groups/ssac/documents-correspondence'
 groups['ssac_cor']['regex'] = []
 groups['ssac_cor']['regex'].append(re.compile('.*/system/files/files/ssac2.*\.pdf$'))
 
 groups['rssac'] = {}
-groups['rssac']['uri'] = []
-groups['rssac']['uri'].append('https://www.icann.org/groups/rssac/documents')
+groups['rssac']['uri'] = 'https://www.icann.org/groups/rssac/documents'
 groups['rssac']['regex'] = []
 groups['rssac']['regex'].append(re.compile('.*/system/files/files/.*rssac-.*\.pdf$'))
 groups['rssac']['regex'].append(re.compile('^/en/groups/rssac/rssac-iana-stewardship-transition-08may14-en.pdf$'))
 
 groups['octo'] = {}
-groups['octo']['uri'] = []
-groups['octo']['uri'].append('https://www.icann.org/resources/pages/octo-publications-2019-05-24-en')
+groups['octo']['uri'] = 'https://www.icann.org/resources/pages/octo-publications-2019-05-24-en'
 groups['octo']['regex'] = []
 groups['octo']['regex'].append(re.compile('.*/octo-.*\.pdf$'))
 
 groups['octo_com'] = {}
-groups['octo_com']['uri'] = []
-groups['octo_com']['uri'].append('https://www.icann.org/resources/pages/octo-commissioned-documents-2020-11-05-en')
+groups['octo_com']['uri'] = 'https://www.icann.org/resources/pages/octo-commissioned-documents-2020-11-05-en'
 groups['octo_com']['regex'] = []
 groups['octo_com']['regex'].append(re.compile('.*/system/files/files/.*\.pdf$'))
 
 groups['ge'] = {}
-groups['ge']['uri'] = []
-groups['ge']['uri'].append('https://www.icann.org/en/government-engagement/publications?page=1')
+groups['ge']['uri'] = 'https://www.icann.org/en/government-engagement/publications?page=1'
 groups['ge']['regex'] = []
 groups['ge']['regex'].append(re.compile('.*/en/files/government-engagement-ge/.*\.pdf$'))
 
 groups['rzerc'] = {}
-groups['rzerc']['uri'] = []
-groups['rzerc']['uri'].append('https://www.icann.org/en/rzerc#documents')
+groups['rzerc']['uri'] = 'https://www.icann.org/en/rzerc#documents'
 groups['rzerc']['regex'] = []
 groups['rzerc']['regex'].append(re.compile('.*/uploads/ckeditor/rzerc-0.*\.pdf$'))
 
+groups['board_cor'] = {}
+groups['board_cor']['sub_dir'] = {} # Before 2003 there are no PDFs
+groups['board_cor']['sub_dir']['2003'] = 'https://www.icann.org/resources/pages/2003-2012-10-11-en'
+groups['board_cor']['sub_dir']['2004'] = 'https://www.icann.org/resources/pages/2004-2012-10-11-en'
+groups['board_cor']['sub_dir']['2005'] = 'https://www.icann.org/resources/pages/2005-2012-10-11-en'
+groups['board_cor']['sub_dir']['2006'] = 'https://www.icann.org/resources/pages/2006-2012-10-11-en'
+groups['board_cor']['sub_dir']['2007'] = 'https://www.icann.org/resources/pages/2007-2012-10-11-en'
+groups['board_cor']['sub_dir']['2008'] = 'https://www.icann.org/resources/pages/2008-2012-10-11-en'
+groups['board_cor']['sub_dir']['2009'] = 'https://www.icann.org/resources/pages/2009-2012-10-11-en'
+groups['board_cor']['sub_dir']['2010'] = 'https://www.icann.org/resources/pages/2010-2012-10-11-en'
+groups['board_cor']['sub_dir']['2011'] = 'https://www.icann.org/resources/pages/2011-2012-02-25-en'
+groups['board_cor']['sub_dir']['2012'] = 'https://www.icann.org/resources/pages/2012-2013-01-10-en'
+groups['board_cor']['sub_dir']['2013'] = 'https://www.icann.org/resources/pages/2013-2014-01-24-en'
+groups['board_cor']['sub_dir']['2014'] = 'https://www.icann.org/resources/pages/2014-2014-01-24-en'
+groups['board_cor']['sub_dir']['2015'] = 'https://www.icann.org/resources/pages/correspondence-2015'
+groups['board_cor']['sub_dir']['2016'] = 'https://www.icann.org/resources/pages/correspondence-2016'
+groups['board_cor']['sub_dir']['2017'] = 'https://www.icann.org/resources/pages/correspondence-2017'
+groups['board_cor']['sub_dir']['2018'] = 'https://www.icann.org/resources/pages/correspondence-2018'
+groups['board_cor']['sub_dir']['2019'] = 'https://www.icann.org/resources/pages/correspondence-2019'
+groups['board_cor']['sub_dir']['2020'] = 'https://www.icann.org/resources/pages/correspondence-2020'
+groups['board_cor']['sub_dir']['2021'] = 'https://www.icann.org/resources/pages/correspondence-2021'
+groups['board_cor']['sub_dir']['2022'] = 'https://www.icann.org/resources/pages/correspondence-2022'
+groups['board_cor']['regex'] = []
+groups['board_cor']['regex'].append(re.compile('.*/correspondence/.*\.pdf$'))
+groups['board_cor']['regex'].append(re.compile('.*/news/correspondence/.*\.pdf$'))
+groups['board_cor']['regex'].append(re.compile('.*/system/files/files/.*\.pdf$'))
+groups['board_cor']['regex'].append(re.compile('.*/system/files/correspondence/.*\.pdf$'))
 
-exclude = []
-exclude.append('/en/system/files/files/didp-response-process-29oct13-en.pdf')
-exclude.append('https://www.icann.org/en/system/files/files/registrar-billing-faq-21dec21-en.pdf')
-exclude.append('/en/system/files/files/gdd-ops-handbook-registry-operators-15aug18-en.pdf')
-exclude.append('/mosapi-specification.pdf')
+groups['gac'] = {}
+groups['gac']['uri'] = 'https://gac.icann.org/contentMigrated/icann1-singapore-communique'
+groups['gac']['option_regex'] = []
+groups['gac']['option_regex'].append(re.compile('^/contentMigrated/icann.*-communique$'))
+groups['gac']['regex'] = []
+groups['gac']['regex'].append(re.compile('^.*/.*communique.*\.pdf[\?language_id.*]?', flags=re.ASCII | re.IGNORECASE))
+
 
 # Grab a file and write to disk
 def download(uri, fname):
-  req = requests.get(uri,stream=True)
-  if req.status_code == 200:
-    with open(fname, 'wb') as f:
-      for chunk in req.iter_content(chunk_size=1024):
-        if chunk: # filter out keep-alive new chunks
-          f.write(chunk)
-  os.chmod(fname, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IROTH) # 0644
-
-
-for gr in groups:
-  links = []
-  for URI in groups[gr]['uri']:
-    req = requests.get(URI, data=None)
+  print(uri + ' ==> ' + fname)
+  try:
+    req = requests.get(uri,stream=True)
     if req.status_code == 200:
-      soup = BeautifulSoup(req.text, 'html.parser')
-      for link in soup.find_all('a'):
-        href = link.get('href')
-        if href is None:
-          continue
-        if href.endswith('.pdf'):
-          #print(href)
-          for reg in groups[gr]['regex']:
-            if reg.match(href):
-              if not href in exclude:
-                if href.startswith('https://'):
-                  links.append(href)
-                else:
-                  links.append('https://www.icann.org' + href)
+      with open(fname, 'wb') as f:
+        for chunk in req.iter_content(chunk_size=1024):
+          if chunk: # filter out keep-alive new chunks
+            f.write(chunk)
+      os.chmod(fname, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IROTH) # 0644
+    else:
+      print("dl_bad_response:" + uri)
+  except requests.RequestException:
+    print("req_exception:" + uri)
 
-  links = list(dict.fromkeys(links)) # Remove duplicates
-  for ll in links:
-    fname = base_dir + gr + '/' + ll.split("/")[-1]
-    if not os.path.exists(fname):
-      print(ll + ' ==> ' + fname)
-      download(ll, fname)
+
+# Grab links in tags matching regex
+# Returns deduplicated list of links
+def get_links(URI, regex, tags=['a', 'href']):
+  def is_excluded(s): # Should we exclude this link?
+    for pattern in exclude:
+      if pattern.match(s):
+        return True
+    return False
+
+  links = []
+  url_t = Util.parse_url(URI)
+
+  req = requests.get(URI)
+  if req.status_code == 200:
+    soup = BeautifulSoup(req.text, 'html.parser')
+    for tag in soup.find_all(tags[0]):
+      link = tag.get(tags[1])
+      if link is None:
+        continue
+      if not is_excluded(link):
+        for reg in regex:
+          if reg.match(link):
+            link = link.split('?')[0] # Strip any trailing garbage
+            if Util.parse_url(link).host is None:
+              links.append(url_t.scheme + '://' + url_t.host + link)
+            elif len(Util.parse_url(link).host) < len('icann.org'): # Relative path missing leading /
+              links.append(url_t.scheme + '://' + url_t.host + '/' + link)
+            else:
+              links.append(link)
+  return list(dict.fromkeys(links))
+
+# BEGIN EXECUTION
+for gr in groups:
+  if gr == 'board_cor':
+    for sub_dir,URI in groups[gr]['sub_dir'].items():
+      for ll in get_links(URI, groups[gr]['regex']):
+        fname = base_dir + gr + '/' + sub_dir + '/' + ll.split('/')[-1]
+        if not os.path.exists(fname):
+          download(ll, fname)
+
+  elif gr == 'gac':
+    for page in get_links(groups[gr]['uri'], groups[gr]['option_regex'], ['option', 'value']):
+      for ll in get_links(page, groups[gr]['regex']):
+        fname = base_dir + gr + '/' + ll.split('/')[-1]
+        if not os.path.exists(fname):
+          download(ll, fname)
+
+  else:
+    for ll in get_links(groups[gr]['uri'], groups[gr]['regex']):
+      fname = base_dir + gr + '/' + ll.split('/')[-1]
+      if not os.path.exists(fname):
+        download(ll, fname)
