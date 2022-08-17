d0bfa7d468dc61f7595e8bb5c2dcaf6c8e7d6817
Andrew McConachie <andrew@depht.com>
2022-08-17 19:39:56 +0000

    Make recursive

--- ocrable.sh
+++ ocrable.sh
@@ -1,7 +1,7 @@
 #!/bin/ksh
-for IMG in $(pdf_info.py -wi $1)
+for IMG in $(pdf_info.py -wir $1)
 do
-	OCR=$(basename $IMG .pdf)_ocr.pdf
+	OCR=$(echo $IMG | rev | cut -d. -f2- | rev)_ocr.pdf
 	if [ ! -f "$OCR" ]; then
 		echo $IMG
 	fi
