e66b085dc9692c7ce040815dac0c1d737e5201a5
Andrew McConachie <andrew@depht.com>
2025-03-31 13:07:16 +0000

    Add staging and announcements

--- yearly.sh
+++ yearly.sh
@@ -1,9 +1,15 @@
 #!/bin/ksh
 # To be executed once in the month of December every year
-BASE=/var/www/htdocs/icannhaz.org/
+BASE=/var/www/htdocs/icannhaz.org
+STAGE=/home/smutt/staging
 YEAR=$(($(date +%Y) + 1))
 
-for DIR in ham/soac/alac/pub ham/icann/cor/ html/icann/blog/
+for DIR in ham/soac/alac/pub ham/icann/cor/ html/icann/blog/ html/icann/announcements
 do
 	mkdir -m 755 $BASE/$DIR/$YEAR
+done
+
+for DIR in html/icann/blog html/icann/announcements
+do
+	mkdir -m 755 $STAGE/$DIR/$YEAR
 done
